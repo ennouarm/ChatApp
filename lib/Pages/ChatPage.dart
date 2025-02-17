8045a3fcb067f8790eb3a9f996176d8ae0741808
ennouarm <ennm05@uqo.ca>
2025-02-16 20:24:51 -0500

    Chat App

--- lib/Pages/ChatPage.dart
+++ lib/Pages/ChatPage.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:newproj3/Services/Chat/chat_services.dart';
 import 'package:newproj3/Services/auth/auth_service.dart';
@@ -27,6 +28,7 @@ class ChatPage extends StatelessWidget {
         children: [
           Expanded(
             child: _buildMesasgeList(),),
+            _buildUserInput(),
         ],
       ),
     );
@@ -43,13 +45,32 @@ class ChatPage extends StatelessWidget {
           }
           //loading
           if(snapshot.connectionState == ConnectionState.waiting){
-            return const Text("...loading")
+            return const Text("...loading");
           }
           //return listview
           return ListView(
-            children: snapshot.data!.docs.map((doc) => _buildMesssageItem(doc).toList()),
+            children: snapshot.data!.docs.map((doc) => _buildMessageItem(doc)).toList(),
           );
         },
+    );
+  }
+  Widget _buildMessageItem (DocumentSnapshot doc){
+    Map<String, dynamic> data = doc.data() as Map <String, dynamic>;
+    return Text(data["message"]);
+  }
+  Widget _buildUserInput(){
+    return Row(
+      children: [
+        Expanded(
+          child: TextField(
+            controller : _messageController,
+            decoration: InputDecoration(
+            hintText : "Type a message",),
+            obscureText : false,
+          ),
+        ),
+        IconButton(onPressed: sendMessage, icon: const Icon(Icons.arrow_circle_up))
+      ],
     );
   }
 }
